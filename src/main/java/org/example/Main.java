1ee22d4d4843199019c528bb470ea1b767f803a7
Wassword <ahmedsaba271@gmail.com>
2024-08-12 10:48:07 -0400

    Final Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,33 +11,33 @@ public class Main {
        FileManager fileManager = new FileManager();
 
         //Directory path
-        String directoryPath = "C:";
+        String directoryPath = "C:/User/Admin";
 
         //Display contents of directory
         fileManager.displayDirectoryContents((directoryPath));
 
         //Source and file path
 
-        String sourceFilePath = "";
-        String destinationPath = "";
+        String sourceFilePath = "C:/User/Admin/.txt";
+        String destinationPath = "C:/User/Admin/file_copy.txt";
 
 
         //Copy a file
         fileManager.copyFile(sourceFilePath, destinationPath);
 
         //Move a file
-        String moveDestinationPath ="";
+        String moveDestinationPath ="C:/User/Admin/moved_file.txt";
         fileManager.moveFile(destinationPath, moveDestinationPath);
 
-        //Delete Fil
+        //Delete File
         fileManager.deleteFile(moveDestinationPath);
 
         //Create new directory
-        String newDirectoryPath = "";
+        String newDirectoryPath = "C:/User/Admin/new_directory";
         fileManager.createDirectory(newDirectoryPath);
 
         ////Search files within directory
-        String searchQuery = "";
+        String searchQuery = ".txt";
         fileManager.searchFiles(directoryPath,searchQuery );
         //Initializing UI
         CLI.startApplication();
